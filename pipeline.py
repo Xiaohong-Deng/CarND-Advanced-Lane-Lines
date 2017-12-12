99bd736808a339995e2ca028eb4f2ed655eb5bd6
Xiaohong-Deng <worldsky86rough@gmail.com>
2017-12-12 22:25:55 +0800

    lane detected and colored

--- pipeline.py
+++ pipeline.py
@@ -2,6 +2,8 @@ import pickle
 import matplotlib.pyplot as plt
 import PIL
 import pipeline_helpers as ph
+import numpy as np
+import cv2
 from importlib import reload
 %matplotlib inline
 
@@ -31,35 +33,24 @@ except FileNotFoundError:
 # save the distorted and undistorted chessboard image, uncomment to save it to your local disk
 # f.savefig('./output_images/calibration_chessboard.jpg')
 
-# plot undistordted road images
-img_bgr = cv2.imread('./test_images/test4.jpg')
+# load and preprocess the image
+img_bgr = cv2.imread('./test_images/test6.jpg')
 img = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2RGB)
 img_gray = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2GRAY)
 undist = cv2.undistort(img, camMat, distCoeffs, None, camMat)
-
-# uncomment to plot undistorted road image
-# plt.figure(figsize=(16, 9))
-# plt.imshow(undist, interpolation='nearest', aspect='auto')
-
 # save the output image, uncomment to save it to your local disk
 # plt.savefig('./output_images/calibration_test1.jpg', bbox_inches='tight')
 
 # STEP2: retrieve a grayscale image only contains lane lines
-# STEP2-a: first let us play with gradients
+# STEP2-a: get the best lane lines captured image we can with gradients
 
-# test our thresholding gradients methods
 ksize = 3
 
 combined_grad = ph.combine_grad(img_gray, ksize=ksize)
-# STEP2-b: now let us play with color space
-
-reload(ph)
+# STEP2-b: get the best lane lines captured image we can with color space
 combined_color, bin_s, bin_l = ph.combine_color(img_bgr)
 combined = np.zeros_like(combined_color)
 combined[(combined_grad == 1) | (combined_color == 1)] = 1
-# uncomment to plot color and gradients combined lane lines binary image
-plt.figure(figsize=(16, 9))
-plt.imshow(combined_grad, interpolation='nearest', aspect='auto', cmap='gray')
 
 # uncomment to save the final combined image to your local disk
 # comb_rescaled = (combined * 255.).astype(np.uint8)
@@ -77,7 +68,7 @@ try:
     perspective_params = pickle.load(f)
     matP, matP_inv = perspective_params['matP'], perspective_params['matP_inv']
 except FileNotFoundError:
-  straight_line_bgr = cv2.imread('./test_images/straight_lines1.jpg')
+  straight_line_bgr = cv2.imread('./test_images/straight_lines2.jpg')
   undist_straight_line = cv2.undistort(straight_line_bgr, camMat, distCoeffs, None, camMat)
 
   # uncomment the following line to save undistorted straight line image to your local disk
@@ -87,18 +78,18 @@ except FileNotFoundError:
   # following are the 4 corners of a rectangle in the unwarped image by eyeballing
   # with some software help
 
-  upper_left = [585, 460]
-  upper_right = [701, 460]
-  lower_left = [211, 719]
+  upper_left = [578, 462]
+  upper_right = [704, 462]
+  lower_left = [212, 719]
   lower_right = [1093, 719]
 
   offset_x = 300
   offset_y = 0
 
   src = np.float32([upper_left, upper_right, lower_right, lower_left])
-  dst = np.float32([[offset_x, offset_y], [img_size[0] - offset_x, offset_y],
-                    [img_size[0] - offset_x, img_size[1] - offset_y],
-                    [offset_x, img_size[1] - offset_y]])
+  dst = np.float32([[offset_x, offset_y], [img_size[0] - offset_x - 1, offset_y],
+                    [img_size[0] - offset_x - 1, img_size[1] - offset_y - 1],
+                    [offset_x, img_size[1] - offset_y - 1]])
 
   matP = cv2.getPerspectiveTransform(src, dst)
   matP_inv = cv2.getPerspectiveTransform(dst, src)
@@ -111,12 +102,12 @@ except FileNotFoundError:
 # test perspective transformation
 warped = cv2.warpPerspective(combined, matP, (img_size[0], img_size[1]))
 
-warped_rescaled = (warped * 255.).astype(np.uint8)
-warped_rescaled = PIL.Image.fromarray(warped_rescaled)
-warped_rescaled.save("./output_images/binary_warped_test1.jpg")
-
-plt.figure(figsize=(16, 9))
-plt.imshow(warped, cmap='gray')
+# warped_rescaled = (warped * 255.).astype(np.uint8)
+# warped_rescaled = PIL.Image.fromarray(warped_rescaled)
+# warped_rescaled.save("./output_images/binary_warped_test1.jpg")
+#
+# plt.figure(figsize=(16, 9))
+# plt.imshow(warped, cmap='gray')
 
 # STEP3-b: now we find the lane lines from the images using convolution
 # draw windows around them, fit a polynomial and draw it on the image
@@ -127,123 +118,8 @@ window_width = 50
 window_height = 180 # Break image into 9 vertical layers since image height is 720
 margin = 100 # How much to slide left and right for searching
 
-# window_mask is used to draw green windows in the image
-def window_mask(width, height, img_ref, center,level):
-  output = np.zeros_like(img_ref)
-  output[int(img_ref.shape[0]-(level+1)*height):int(img_ref.shape[0]-level*height),
-          max(0,int(center-width/2)):min(int(center+width/2),
-                                         img_ref.shape[1])] = 1
-  return output
-
-def find_lane_line_pixels(image, window_width, window_height, margin):
-
-  window_centroids = [] # Store the (left,right) window centroid positions per level
-  window = np.ones(window_width) # Create our window template that we will use for convolutions
-
-  # First find the two starting positions for the left and right lane by using np.sum to get the vertical image slice
-  # and then np.convolve the vertical image slice with the window template
-
-  # Sum quarter bottom of image to get slice, could use a different ratio
-  # image is a grayscale, looking at lower left quarter
-  l_sum = np.sum(image[int(image.shape[0]/2):,:int(image.shape[1]/2)], axis=0)
-  l_center = np.argmax(np.convolve(window,l_sum))-window_width/2
-  # the lower right quarter
-  r_sum = np.sum(image[int(image.shape[0]/2):,int(image.shape[1]/2):], axis=0)
-  # the convolution starts from index 0, so we shift it by half of the width
-  r_center = np.argmax(np.convolve(window,r_sum))-window_width/2+int(image.shape[1]/2)
-
-  # note l_center and r_center are x coordinates in the image
-
-  # Add what we found for the first layer
-  # this is our first window
-  window_centroids.append((l_center, r_center))
-
-  # still we need to collect all the nonzero pixels in this window
-  # so later we can fit a polynomial
-  # here the window width used to collect pixels is 2 * margin
-  nonzero = image.nonzero()
-  nonzeroy = nonzero[0]
-  nonzerox = nonzero[1]
-
-  left_lane_inds = []
-  right_lane_inds = []
-
-  win_y_low = image.shape[0] - window_height
-  win_y_high = image.shape[0]
-
-  win_x_l_low = l_center - margin
-  win_x_l_high = l_center + margin
-  win_x_r_low = r_center - margin
-  win_x_r_high = r_center + margin
-
-  good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
-                    (nonzerox >= win_x_l_low) & (nonzerox < win_x_l_high)).nonzero()[0]
-  good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
-                     (nonzerox >= win_x_r_low) & (nonzerox < win_x_r_high)).nonzero()[0]
-
-  left_lane_inds.append(good_left_inds)
-  right_lane_inds.append(good_right_inds)
-
-  # Go through each layer looking for max pixel locations
-  for level in range(1,(int)(image.shape[0]/window_height)):
-    # convolve the window into the vertical slice of the image
-    # in the loop we go through the entire width
-    image_layer = np.sum(image[int(image.shape[0]-(level+1)*window_height):int(image.shape[0]-level*window_height),:], axis=0)
-    conv_signal = np.convolve(window, image_layer)
-    # Find the best left centroid by using past left center as a reference
-    # Use window_width/2 as offset because convolution signal reference is at right side of window, not center of window
-    offset = window_width/2
-    # to avoid negative index, use max()
-    # it is the index in conv_signal
-    l_min_index = int(max(l_center+offset-margin,0))
-    # to avoid index larger than width, use min()
-    l_max_index = int(min(l_center+offset+margin,image.shape[1]))
-    # get the index in original image
-    l_center = np.argmax(conv_signal[l_min_index:l_max_index])+l_min_index-offset
-    # Find the best right centroid by using past right center as a reference
-    r_min_index = int(max(r_center+offset-margin,0))
-    r_max_index = int(min(r_center+offset+margin,image.shape[1]))
-    r_center = np.argmax(conv_signal[r_min_index:r_max_index])+r_min_index-offset
-    # Add what we found for that layer
-    window_centroids.append((l_center,r_center))
-
-    win_y_low = image.shape[0] - (level + 1) * window_height
-    win_y_high = image.shape[0] - level * window_height
-
-    win_x_l_low = l_center - margin
-    win_x_l_high = l_center + margin
-    win_x_r_low = r_center - margin
-    win_x_r_high = r_center + margin
-
-    good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
-                      (nonzerox >= win_x_l_low) & (nonzerox < win_x_l_high)).nonzero()[0]
-    good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
-                       (nonzerox >= win_x_r_low) & (nonzerox < win_x_r_high)).nonzero()[0]
-
-    left_lane_inds.append(good_left_inds)
-    right_lane_inds.append(good_right_inds)
-
-  left_lane_inds = np.concatenate(left_lane_inds)
-  right_lane_inds = np.concatenate(right_lane_inds)
-  # centroids we get is the x coordinates for n windows
-  # Extract left and right line pixel positions
-  leftx = nonzerox[left_lane_inds]
-  lefty = nonzeroy[left_lane_inds]
-  rightx = nonzerox[right_lane_inds]
-  righty = nonzeroy[right_lane_inds]
-
-  # Fit a second order polynomial to each
-  left_fit = np.polyfit(lefty, leftx, 2)
-  right_fit = np.polyfit(righty, rightx, 2)
-
-  # Generate x and y values for plotting
-  ploty = np.linspace(0, image.shape[0]-1, image.shape[0])
-  left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
-  right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
-
-  return leftx, lefty, rightx, righty
 # find the lane lines centers in the bird eye's perspective
-leftx, lefty, rightx, righty = find_lane_line_pixels(warped, window_width, window_height, margin)
+leftx, lefty, rightx, righty = ph.find_lane_line_pixels(warped, window_width, window_height, margin)
 
 # Fit a second order polynomial to each
 left_fit = np.polyfit(lefty, leftx, 2)
@@ -289,7 +165,7 @@ plt.plot(right_fitx, ploty, color='yellow') # center line for right line in yell
 plt.xlim(0, 1280)
 plt.ylim(720, 0)
 
-plt.savefig('./output_images/line_pixels_poly_fit_test4.jpg', bbox_inches='tight')
+# plt.savefig('./output_images/line_pixels_poly_fit_test4.jpg', bbox_inches='tight')
 
 ym_per_pix = 30/720 # meters per pixel in y dimension
 xm_per_pix = 3.7/700 # meters per pixel in x dimension
@@ -304,4 +180,43 @@ left_curverad = ((1 + (2*left_fit_cr[0]*y_eval*ym_per_pix + left_fit_cr[1])**2)*
 right_curverad = ((1 + (2*right_fit_cr[0]*y_eval*ym_per_pix + right_fit_cr[1])**2)**1.5) / np.absolute(2*right_fit_cr[0])
 # Now our radius of curvature is in meters
 print(left_curverad, 'm', right_curverad, 'm')
-# Example values: 632.1 m    626.2 m
+
+# also I want to now where the car center is in the warped image
+# car center is assumed to be horizontally in the center of the unwarped image
+car_center = np.zeros_like(combined)
+car_center[car_center.shape[0] - 1, car_center.shape[1] // 2 - 1] = 1
+car_center_warp = cv2.warpPerspective(car_center, matP, (img_size[0], img_size[1]))
+car_centerx = np.argmax(car_center_warp[car_center_warp.shape[0] - 1, :])
+lane_centerx = ((right_fitx[-1] + left_fitx[-1]) // 2)
+
+car_offset_meters = (car_centerx - lane_centerx) * xm_per_pix
+print(car_offset_meters, 'm')
+
+# Let us try to draw the lane in green and warp it back to the original perspective
+# Create an image to draw the lines on
+warp_zero = np.zeros_like(warped).astype(np.uint8)
+color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
+
+# Recast the x and y points into usable format for cv2.fillPoly()
+pts_left = np.array([np.transpose(np.vstack([left_fitx, ploty]))])
+pts_right = np.array([np.flipud(np.transpose(np.vstack([right_fitx, ploty])))])
+pts = np.hstack((pts_left, pts_right))
+
+# Draw the lane onto the warped blank image
+cv2.fillPoly(color_warp, np.int_([pts]), (0,255, 0))
+
+# Warp the blank back to original image space using inverse perspective matrix (Minv)
+
+# Combine the result with the original image
+newwarp = cv2.warpPerspective(color_warp, matP_inv, (img.shape[1], img.shape[0]))
+result = cv2.addWeighted(undist, 1, newwarp, 0.3, 0)
+
+font = cv2.FONT_HERSHEY_SIMPLEX
+result = cv2.putText(result,"left line curvature: " + str(left_curverad) + " meters", (20,40), font, 1, (255,255,255), 2, cv2.LINE_AA)
+result = cv2.putText(result,"right line curvature: " + str(right_curverad) + " meters", (20,80), font, 1, (255,255,255), 2, cv2.LINE_AA)
+result = cv2.putText(result,"car offset: " + str(car_offset_meters) + " meters" , (20,120), font, 1, (255,255,255), 2, cv2.LINE_AA)
+
+plt.figure(figsize=(16, 9))
+plt.imshow(result)
+
+# cv2.imwrite('./output_images/lane_detected_test4.jpg', cv2.cvtColor(result, cv2.COLOR_RGB2BGR))
--- pipeline_helpers.py
+++ pipeline_helpers.py
@@ -2,11 +2,23 @@ import numpy as np
 import cv2
 
 # generate camera calibration parameters
-def get_undist_params():
-  fn_prefix = './camera_cal/calibration'
+def get_undist_params(fn_prefix='./camera_cal/calibration', nx=9, ny=6):
+  """
+  Compute parameters needed to undistort the distorted image
+
+  Input
+  -----
+  fn_prefix : file name prefix which should be the path to the calibration chessboard images
+
+  nx : number of corners in each row of a chessboard image
+
+  ny : number of corners in eahc column of a chessboard image
+
+  Output
+  -----
+  A 5-element tuple containing objects of different types
+  """
   fnames = []
-  nx = 9
-  ny = 6
   objpoints = []
   imgpoints = []
   objp = np.zeros((nx*ny, 3), np.float32)
@@ -33,6 +45,23 @@ def get_undist_params():
 
 # the following methods are for gradient and color thresholding
 def abs_sobel_thresh(gray, orient='x', sobel_kernel=3, thresh=(0, 255)):
+  """
+  Compute binary grayscaled image that captures the lane lines
+
+  Input
+  -----
+  gray : a gray image
+
+  orient : the axis along which you compute your gradients
+
+  sobel_kernel : the kernel size passed into Sobel()
+
+  thresh : thresholds used to exclude noises
+
+  Output
+  -----
+  A binary grayscaled image
+  """
   if orient == 'x':
     sobel = cv2.Sobel(gray, cv2.CV_64F, 1, 0, ksize=sobel_kernel)
   elif orient == 'y':
@@ -71,6 +100,25 @@ def dir_thresh(gray, sobel_kernel=3, thresh=(0, np.pi/2)):
   return binary_output
 
 def combine_grad(gray, ksize=3, grad_thresh=(20, 100), magnitude_thresh=(30, 100), direction_thresh=(0.7, 1.57)):
+  """
+  Compute binary grayscaled image that captures the lane lines
+
+  Input
+  -----
+  gray : a gray image
+
+  ksize : kernel size to pass into other methods called in this method
+
+  grad_thresh : thresholds passed into abs_sobel_thresh()
+
+  magnitude_thresh : threhsolds passed into mag_thresh()
+
+  direction_thresh : thresholds passed into dir_thresh()
+
+  Output
+  -----
+  A binary grayscaled image
+  """
   gradx = abs_sobel_thresh(gray, orient='x', sobel_kernel=ksize, thresh=grad_thresh)
   grady = abs_sobel_thresh(gray, orient='y', sobel_kernel=ksize, thresh=grad_thresh)
   mag_binary = mag_thresh(gray, sobel_kernel=ksize, thresh=magnitude_thresh)
@@ -82,6 +130,21 @@ def combine_grad(gray, ksize=3, grad_thresh=(20, 100), magnitude_thresh=(30, 100
   return combined
 
 def combine_color(img_bgr, rgb_thresh=(220, 255), hls_thresh=(90, 255)):
+  """
+  Compute binary grayscaled image that captures the lane lines
+
+  Input
+  -----
+  img_bgr : image in BGR form
+
+  rgb_thresh : thresholds for R, G, B channel images to exclude noises
+
+  hls_thresh : thresholds for H, L, S channel images to exclude noises
+
+  Output
+  -----
+  A binary grayscaled image
+  """
   img_r = img_bgr[:, :, 2]
   binary_r = np.zeros_like(img_r)
   binary_r[(img_r > rgb_thresh[0]) & (img_r <= rgb_thresh[1])] = 1
@@ -98,3 +161,138 @@ def combine_color(img_bgr, rgb_thresh=(220, 255), hls_thresh=(90, 255)):
   combined[((binary_s == 1) & (binary_l == 1)) | (binary_r == 1)] = 1
 
   return combined, binary_s, binary_l
+
+# I didn't use window_mask method in my pipeline, skip reading it if you want
+# window_mask is used to draw green windows on the lane lines in the image
+def window_mask(width, height, img_ref, center,level):
+  output = np.zeros_like(img_ref)
+  output[int(img_ref.shape[0]-(level+1)*height):int(img_ref.shape[0]-level*height),
+          max(0,int(center-width/2)):min(int(center+width/2),
+                                         img_ref.shape[1])] = 1
+  return output
+
+def find_lane_line_pixels(image, window_width, window_height, margin):
+  """
+  Computes all the coordinates for the pixels that constitute the lane lines
+  Use convolution to find window centroid. Then the lane line pixels
+
+  Inputs
+  -----
+  image : presumably it is a binary grayscaled image, with elements of only 0 or 1
+
+  window_width : covolution window width of your choice
+
+  window_height : covolution widnow height of your choice
+
+  margin : the horizontal offset from the window centroids we use to draw a bounding box
+           at each level to find lane line pixels. Dont confuse it with covolution window width
+
+  Outputs
+  -----
+  a 4-element tuple containing x coordinates and y coordinates for left and right lane lines
+  """
+  window_centroids = [] # Store the (left,right) window centroid positions per level
+  window = np.ones(window_width) # Create our window template that we will use for convolutions
+
+  # First find the two starting positions for the left and right lane by using np.sum to get the vertical image slice
+  # and then np.convolve the vertical image slice with the window template
+
+  # Sum quarter bottom of image to get slice, could use a different ratio
+  # image is a grayscale, looking at lower left quarter
+  l_sum = np.sum(image[int(image.shape[0]/2):,:int(image.shape[1]/2)], axis=0)
+  l_center = np.argmax(np.convolve(window,l_sum))-window_width/2
+  # the lower right quarter
+  r_sum = np.sum(image[int(image.shape[0]/2):,int(image.shape[1]/2):], axis=0)
+  # the convolution starts from index 0, so we shift it by half of the width
+  r_center = np.argmax(np.convolve(window,r_sum))-window_width/2+int(image.shape[1]/2)
+
+  # note l_center and r_center are x coordinates in the image
+
+  # Add what we found for the first layer
+  # this is our first window
+  window_centroids.append((l_center, r_center))
+
+  # still we need to collect all the nonzero pixels in this window
+  # so later we can fit a polynomial
+  # here the window width used to collect pixels is 2 * margin
+  nonzero = image.nonzero()
+  nonzeroy = nonzero[0]
+  nonzerox = nonzero[1]
+
+  left_lane_inds = []
+  right_lane_inds = []
+
+  win_y_low = image.shape[0] - window_height
+  win_y_high = image.shape[0]
+
+  win_x_l_low = l_center - margin
+  win_x_l_high = l_center + margin
+  win_x_r_low = r_center - margin
+  win_x_r_high = r_center + margin
+
+  good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                    (nonzerox >= win_x_l_low) & (nonzerox < win_x_l_high)).nonzero()[0]
+  good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                     (nonzerox >= win_x_r_low) & (nonzerox < win_x_r_high)).nonzero()[0]
+
+  left_lane_inds.append(good_left_inds)
+  right_lane_inds.append(good_right_inds)
+
+  # Go through each layer looking for max pixel locations
+  for level in range(1,(int)(image.shape[0]/window_height)):
+    # convolve the window into the vertical slice of the image
+    # in the loop we go through the entire width
+    image_layer = np.sum(image[int(image.shape[0]-(level+1)*window_height):int(image.shape[0]-level*window_height),:], axis=0)
+    conv_signal = np.convolve(window, image_layer)
+    # Find the best left centroid by using past left center as a reference
+    # Use window_width/2 as offset because convolution signal reference is at right side of window, not center of window
+    offset = window_width/2
+    # to avoid negative index, use max()
+    # it is the index in conv_signal
+    l_min_index = int(max(l_center+offset-margin,0))
+    # to avoid index larger than width, use min()
+    l_max_index = int(min(l_center+offset+margin,image.shape[1]))
+    # get the index in original image
+    l_center = np.argmax(conv_signal[l_min_index:l_max_index])+l_min_index-offset
+    # Find the best right centroid by using past right center as a reference
+    r_min_index = int(max(r_center+offset-margin,0))
+    r_max_index = int(min(r_center+offset+margin,image.shape[1]))
+    r_center = np.argmax(conv_signal[r_min_index:r_max_index])+r_min_index-offset
+    # Add what we found for that layer
+    window_centroids.append((l_center,r_center))
+
+    win_y_low = image.shape[0] - (level + 1) * window_height
+    win_y_high = image.shape[0] - level * window_height
+
+    win_x_l_low = l_center - margin
+    win_x_l_high = l_center + margin
+    win_x_r_low = r_center - margin
+    win_x_r_high = r_center + margin
+
+    good_left_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                      (nonzerox >= win_x_l_low) & (nonzerox < win_x_l_high)).nonzero()[0]
+    good_right_inds = ((nonzeroy >= win_y_low) & (nonzeroy < win_y_high) &
+                       (nonzerox >= win_x_r_low) & (nonzerox < win_x_r_high)).nonzero()[0]
+
+    left_lane_inds.append(good_left_inds)
+    right_lane_inds.append(good_right_inds)
+
+  left_lane_inds = np.concatenate(left_lane_inds)
+  right_lane_inds = np.concatenate(right_lane_inds)
+  # centroids we get is the x coordinates for n windows
+  # Extract left and right line pixel positions
+  leftx = nonzerox[left_lane_inds]
+  lefty = nonzeroy[left_lane_inds]
+  rightx = nonzerox[right_lane_inds]
+  righty = nonzeroy[right_lane_inds]
+
+  # Fit a second order polynomial to each
+  left_fit = np.polyfit(lefty, leftx, 2)
+  right_fit = np.polyfit(righty, rightx, 2)
+
+  # Generate x and y values for plotting
+  ploty = np.linspace(0, image.shape[0]-1, image.shape[0])
+  left_fitx = left_fit[0]*ploty**2 + left_fit[1]*ploty + left_fit[2]
+  right_fitx = right_fit[0]*ploty**2 + right_fit[1]*ploty + right_fit[2]
+
+  return leftx, lefty, rightx, righty
